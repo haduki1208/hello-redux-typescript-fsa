4d115189e46e059def755f70cbfb1481edeb9672
haduki1208 <bgst1260204@gn.iwasaki.ac.jp>
2019-07-03 09:50:33 +0900

    scss使ってなかったのでimportする

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/';
 import Counter from './Counter/';
+import '../scss/index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
